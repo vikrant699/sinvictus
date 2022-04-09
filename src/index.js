10e4192ad5fda5e36114de40a128f50089ed1c2e
Vikrant Arya <vikrantarya1@gmail.com>
2022-04-09 16:47:48 +0530

    NavBar: 90% done, finalise later

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import LandingPage from './landingPage';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
 import theme from './theme';
+import 'focus-visible/dist/focus-visible';
 
 ReactDOM.render(
   <StrictMode>
--- src/landingPage/Navbar.js
+++ src/landingPage/Navbar.js
@@ -1,5 +1,4 @@
 import {
-  Avatar,
   Box,
   Button,
   ButtonGroup,
@@ -8,17 +7,32 @@ import {
   HStack,
   IconButton,
   useBreakpointValue,
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+  Stack,
 } from '@chakra-ui/react';
 import * as React from 'react';
-import { FiHelpCircle, FiMenu, FiSearch, FiSettings } from 'react-icons/fi';
+import { FiMenu } from 'react-icons/fi';
+import { FaWhatsapp, FaTwitter, FaInstagram, FaFacebook } from 'react-icons/fa';
 import { Logo } from './Logo';
 import { ColorModeSwitcher } from '../ColorModeSwitcher';
 
 export const Navbar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const btnRef = React.useRef();
+
   const isDesktop = useBreakpointValue({
     base: false,
     lg: true,
   });
+
   return (
     <Box
       as="section"
@@ -39,7 +53,7 @@ export const Navbar = () => {
             <HStack spacing="4">
               <Logo />
               {isDesktop && (
-                <ButtonGroup variant="ghost-on-accent" spacing="1">
+                <ButtonGroup variant="ghost" spacing="1">
                   <Button>Home</Button>
                   <Button>Services</Button>
                   <Button>Testimonials</Button>
@@ -51,14 +65,92 @@ export const Navbar = () => {
             {isDesktop ? (
               <HStack spacing="4">
                 <ColorModeSwitcher />
-                <Button>Take a Demo</Button>
+                <Button>Book a Demo</Button>
               </HStack>
             ) : (
-              <IconButton
-                variant="ghost-on-accent"
-                icon={<FiMenu fontSize="1.25rem" />}
-                aria-label="Open Menu"
-              />
+              <>
+                <HStack spacing="4">
+                  <ColorModeSwitcher />
+                  <IconButton
+                    onClick={onOpen}
+                    variant="ghost"
+                    icon={<FiMenu fontSize="1.25rem" />}
+                    aria-label="Open Menu"
+                  />
+                </HStack>
+                <Drawer
+                  isOpen={isOpen}
+                  placement="right"
+                  onClose={onClose}
+                  finalFocusRef={btnRef}
+                >
+                  <DrawerOverlay />
+                  <DrawerContent>
+                    <DrawerHeader>
+                      <Logo />
+                      <DrawerCloseButton />
+                    </DrawerHeader>
+
+                    <DrawerBody>
+                      <Stack spacing="14px">
+                        <Button variant="ghost" isFullWidth="true">
+                          Home
+                        </Button>
+                        <Button variant="ghost" isFullWidth="true">
+                          Services
+                        </Button>
+                        <Button variant="ghost" isFullWidth="true">
+                          Testimonials
+                        </Button>
+                        <Button variant="ghost" isFullWidth="true">
+                          Pricing
+                        </Button>
+                        <Button variant="ghost" isFullWidth="true">
+                          About
+                        </Button>
+                        <Button isFullWidth="true" onClose={onClose}>
+                          Book a Demo
+                        </Button>
+                      </Stack>
+                    </DrawerBody>
+
+                    <DrawerFooter>
+                      <IconButton
+                        variant="ghost"
+                        aria-label="WhatsApp"
+                        size="md"
+                        fontSize="lg"
+                        isRound="true"
+                        icon={<FaWhatsapp />}
+                      />
+                      <IconButton
+                        variant="ghost"
+                        aria-label="Twitter"
+                        size="md"
+                        fontSize="lg"
+                        isRound="true"
+                        icon={<FaTwitter />}
+                      />
+                      <IconButton
+                        variant="ghost"
+                        aria-label="Instagram"
+                        size="md"
+                        fontSize="lg"
+                        isRound="true"
+                        icon={<FaInstagram />}
+                      />
+                      <IconButton
+                        variant="ghost"
+                        aria-label="Facebook"
+                        size="md"
+                        fontSize="lg"
+                        isRound="true"
+                        icon={<FaFacebook />}
+                      />
+                    </DrawerFooter>
+                  </DrawerContent>
+                </Drawer>
+              </>
             )}
           </Flex>
         </Container>
